ad2e3db940add0400cf68096e2ad9e276759232f
mujdecisy <mujdecisy@gmail.com>
2023-10-18 23:44:11 +0300

    chore: udpate next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
-}
+  output: "export",
+  optimizeCss: false,
+  enableBabelRuntime: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

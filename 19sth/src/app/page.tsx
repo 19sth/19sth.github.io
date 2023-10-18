3332411e1cc8c4cce6908a91e48cd899c328acf1
mujdecisy <mujdecisy@gmail.com>
2023-10-18 22:33:24 +0300

    feat: init project

---
+++ 19sth/src/app/page.tsx
@@ -0,0 +1,87 @@
+export default function Home() {
+  const apps = [
+    {
+      title: "Memorise",
+      description: "Study with cards, train your knowledge.",
+      link: "https://19sth.github.io/pwa-memorise/",
+    },
+    {
+      title: "Check",
+      description: "Define, control and track your repeated tasks.",
+      link: "https://19sth.github.io/pwa-check/",
+    },
+    {
+      title: "Glyph",
+      description: "Encrypt and save your notes and credentials.",
+      link: "https://19sth.github.io/pwa-glyph/",
+    },
+    {
+      title: "Whoami",
+      description: "Play whoami that has variaty of topics with your friends.",
+      link: "https://19sth.github.io/pwa-whoami/",
+    },
+  ];
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center  pb-6 pt-8 lg:static lg:w-auto lg:p-4"></p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://github.com/19sth"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            an open-source organization
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial">
+        <div className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
+          <span className="text-7xl text-black">19</span>
+          <span className="text-7xl font-thin border-b-8 border-yellow-300/25 text-black">
+            something
+          </span>
+        </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {apps.map((e) => (
+          <a
+            key={`app_${e.title}`}
+            href={e.link}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              {`${e.title} `}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              {e.description}
+            </p>
+          </a>
+        ))}
+      </div>
+    </main>
+  );
+}
